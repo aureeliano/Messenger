118870f94010027455e03d74862ac7f0ebd6c78c
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2023-04-16 18:54:13 -0300

    cambios al entry point

--- proyecto-AyD/src/main/java/com/grupo/proyecto_AyD/App.java
+++ proyecto-AyD/src/main/java/com/grupo/proyecto_AyD/App.java
@@ -1,13 +1,15 @@
 package com.grupo.proyecto_AyD;
 
+import com.grupo.proyecto_AyD.vistas.MainMenu;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        MainMenu mainMenu = new MainMenu();
     }
 }
