783ec944a956e549b8bb90f3d0fbccf2a9d8b559
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2023-04-25 01:08:20 -0300

    fix instant

--- proyecto-AyD/src/main/java/com/grupo/proyecto_AyD/modelo/Mensaje.java
+++ proyecto-AyD/src/main/java/com/grupo/proyecto_AyD/modelo/Mensaje.java
@@ -4,17 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.Date;
 import java.util.UUID;
 
 @Getter
 @Setter
 public class Mensaje {
-    private Instant fecha;
+    private Date fecha;
     private String mensaje;
     private String id;
 
     public Mensaje(String mensaje) {
-        this.fecha = Instant.now();
+        this.fecha = Date.from(Instant.now());
         this.mensaje = mensaje;
         this.id = UUID.randomUUID().toString();
     }
