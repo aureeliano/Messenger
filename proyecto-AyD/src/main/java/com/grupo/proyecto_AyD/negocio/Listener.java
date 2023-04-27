708f8ade428ed0cedee469b6b225a6505f54e590
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2023-04-27 03:05:10 -0300

    listener cambios

--- proyecto-AyD/src/main/java/com/grupo/proyecto_AyD/negocio/Listener.java
+++ proyecto-AyD/src/main/java/com/grupo/proyecto_AyD/negocio/Listener.java
@@ -3,6 +3,7 @@ package com.grupo.proyecto_AyD.negocio;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.grupo.proyecto_AyD.controlador.ControladorChat;
 import com.grupo.proyecto_AyD.dtos.UsuarioDTO;
+import com.grupo.proyecto_AyD.modelo.Mensaje;
 import com.grupo.proyecto_AyD.modelo.Sesion;
 import com.grupo.proyecto_AyD.modelo.Usuario;
 import com.grupo.proyecto_AyD.tipos.EstadoUsuario;
@@ -11,9 +12,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.URL;
+import java.net.*;
 
 public class Listener implements ChatInterface {
     private ServerSocket serverSocket;
@@ -30,15 +29,16 @@ public class Listener implements ChatInterface {
         this.usuario = Usuario.getUsuario();
 
         try {
+            usuario.setIp(InetAddress.getLocalHost().getHostAddress());
+
             serverSocket = new ServerSocket(puerto);
+
             this.eschuchando = true;
             mapper = new ObjectMapper();
 
-            URL url = new URL("http://checkip.amazonaws.com/");
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            usuario.setIp(br.readLine());
-
             escuchar();
+
+            System.out.println("Escuchando en puerto: " + usuario.getPuerto());
         } catch (IOException e) {
             System.out.println("Error al iniciar el listener: " + e.getMessage());
             throw new RuntimeException(e);
@@ -54,12 +54,17 @@ public class Listener implements ChatInterface {
         Thread thread = new Thread(() -> {
             try {
                 while (eschuchando) {
+                    System.out.println("Esperando conexion..." + serverSocket.toString());
                     Socket soc = serverSocket.accept();
                     out = new PrintWriter(soc.getOutputStream(), true);
                     in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 
-                    if (in.readLine().contains("[CONTROL]")) {
-                        if (in.readLine().contains("[INICIAR_CHAT]")) {
+                    Mensaje mensaje = mapper.readValue(in.readLine(), Mensaje.class);
+
+                    System.out.println("MENSAJE RECIBIDO: " + mensaje.getMensaje());
+
+                    if (mensaje.getMensaje().contains("[CONTROL]")) {
+                        if (mensaje.getMensaje().contains("[INICIAR_CHAT]")) {
                             ControladorChat.getControlador();
                         }
                     }
